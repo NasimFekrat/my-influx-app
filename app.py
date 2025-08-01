6c847b5f49fbd0d7554b299cf9c031d2f187eb3b
Nasim Fekrat <nasim.fekrat@crosslinxtransit.ca>
2025-08-01 12:12:52 -0400

    Remove Deta & Railway deployment config; back to local Flask

--- app.py
+++ app.py
@@ -83,5 +83,4 @@ def fetch_chart_data_url():
     return fetch_chart_data_10min()
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True)
